e0ae746d14e000088f3ff9d7b00a3668e0ded509
bxlouis <bxlouis@users.noreply.github.com>
2020-05-09 13:49:36 +0200

    Update fetch_playlists.sh

--- fetch_playlists.sh
+++ fetch_playlists.sh
@@ -9,11 +9,11 @@ mkdir $RES_PATH
 COUNT=`ls -l $RES_PATH/tvos*.json | wc -l`
 
 # Skip download if files already exist
-if [ $COUNT != 3 ]
+if [ $COUNT != 1 ]
 then
-    wget http://a1.phobos.apple.com/us/r1000/000/Features/atv/AutumnResources/videos/entries.json -O $RES_PATH/tvos10.json
-    wget https://sylvan.apple.com/Aerials/2x/entries.json -O $RES_PATH/tvos11.json --no-check-certificate
-    wget https://sylvan.apple.com/Aerials/resources.tar -qO - --no-check-certificate | tar -xf - entries.json --to-stdout > $RES_PATH/tvos12.json
-    wget https://sylvan.apple.com/Aerials/resources-13.tar -qO - --no-check-certificate | tar -xf - entries.json --to-stdout > $RES_PATH/tvos13.json
+#    wget http://a1.phobos.apple.com/us/r1000/000/Features/atv/AutumnResources/videos/entries.json -O $RES_PATH/tvos10.json
+#    wget https://sylvan.apple.com/Aerials/2x/entries.json -O $RES_PATH/tvos11.json --no-check-certificate
+#    wget https://sylvan.apple.com/Aerials/resources.tar -qO - --no-check-certificate | tar -xf - entries.json --to-stdout > $RES_PATH/tvos12.json
+    wget http://10.0.1.50/aerials/tvos13.json -O $RES_PATH/tvos13.json
 fi
 
